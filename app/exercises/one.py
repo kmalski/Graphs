2f5d3bf70f7107688910ea5908699e2828e101a0
Vectrom <daplociennik@gmail.com>
2020-03-07 18:06:55 +0100

    Add drawing graph in exercise one

--- app/exercises/one.py
+++ app/exercises/one.py
@@ -7,6 +7,7 @@ import pathlib
 from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import ttk
+from math import cos, sin, pi
 
 
 class ExerciseOneTab(ttk.Frame):
@@ -16,6 +17,7 @@ class ExerciseOneTab(ttk.Frame):
         self.graph = None
 
         self.add_buttons()
+        self.add_canvas()
 
     def add_buttons(self):
         btn_frame = ttk.Frame(self)
@@ -32,6 +34,13 @@ class ExerciseOneTab(ttk.Frame):
 
         inc_matrix_btn = ttk.Button(btn_frame, text='Konwertuj na macierz incydencji', command=self.convert_to_inc_matrix)
         inc_matrix_btn.grid(row=1, column=2)
+
+        draw_graph_btn = ttk.Button(btn_frame, text='Rysuj graf', command=self.draw_graph)
+        draw_graph_btn.grid(row=1, column=3)
+
+    def add_canvas(self):
+        self.canvas = tk.Canvas(self, width=400, height=400)
+        self.canvas.grid(row=2, column=0)
 
     def load_graph(self, event=None):
         self.graph = None
@@ -69,3 +78,42 @@ class ExerciseOneTab(ttk.Frame):
         except(AttributeError):
             messagebox.showinfo(title='Wykrzyknik!', message='Graf w tej formie został wczytany z pliku!')
         
+    def create_circle(self, canvasName, x, y, r):
+        x0 = x - r
+        y0 = y - r
+        x1 = x + r
+        y1 = y + r
+        return canvasName.create_oval(x0, y0, x1, y1, fill="white")
+
+    def draw_graph(self):
+        if not self.graph:
+            messagebox.showinfo(title='Wykrzyknik!', message='Musisz najpierw wczytać jakiś graf!')
+        
+        self.canvas.delete("all")
+
+        graph_to_draw = self.graph
+        if not isinstance(self.graph, AdjacencyList):
+            graph_to_draw = self.graph.to_adjacency_list()
+
+        n = len(graph_to_draw.graph)
+        center = (self.canvas.winfo_width() / 2, self.canvas.winfo_height() / 2) #needed to convert from cartesian to screen coordinates
+        r = min(center) / 1.5
+
+        diff_angle = 2 * pi / n
+        for i in range(n):
+            #calculate node coordinates
+            angle = diff_angle * i
+            x = center[0] + r * cos(angle)
+            y = center[1] - r * sin(angle)
+
+            #drawing edges
+            for neighbour in graph_to_draw.graph[i]:
+                if neighbour <= i:
+                    continue
+                neighbour_angle = diff_angle * neighbour
+                neighbour_x = center[0] + r * cos(neighbour_angle)
+                neighbour_y = center[1] - r * sin(neighbour_angle)
+                self.canvas.create_line(x, y, neighbour_x, neighbour_y)
+
+            self.create_circle(self.canvas, x, y, r * 0.2)
+            self.canvas.create_text(x, y, text=str(i))
