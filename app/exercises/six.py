e94c1a3688b7a81f47a63d72a1e3b6025aa452ee
Damian Płóciennik <damian.plociennik@mybboard.pl>
2020-05-11 13:59:01 +0200

    Add simulated annealing (last task) (#27)

--- app/exercises/six.py
+++ app/exercises/six.py
@@ -1,11 +1,13 @@
 from exercises.base import BaseTab
 from utils.graph_utils import generate_pagerank_graph
 from structures.adjacency_list import DirectedAdjacencyList
+from structures.coordinated_adjacency_list import CoordinatedAdjacencyList
 
 import networkx as nx
 import tkinter as tk
 import random
-from tkinter import ttk, messagebox
+from typing import Dict
+from tkinter import ttk, filedialog, messagebox
 
 
 class ExerciseSixTab(BaseTab):
@@ -52,11 +54,18 @@ class ExerciseSixTab(BaseTab):
         
         ########################### 2 ###########################
 
-        ttk.Button(menu_frame, text='Wczytaj plik...', width=30, command=self.load_file)\
+        ttk.Button(menu_frame, text='Wczytaj plik...', width=30, command=self.load_from_file)\
             .grid(row=7, column=0, columnspan=2, pady=3)
 
+        ttk.Label(menu_frame, text='Liczba iteracji').grid(row=8, column=0)
+        self.max_it_entry = ttk.Entry(menu_frame, width=10)
+        self.max_it_entry.grid(row=9, column=0, pady=2, padx=2)
+
+        ttk.Button(menu_frame, text='Symulowane wyżarzanie', width=30, command=self.simulated_annealing)\
+            .grid(row=10, column=0, columnspan=2, pady=3)
+
         ttk.Separator(menu_frame, orient='horizontal')\
-            .grid(row=8, column=0, columnspan=2, sticky='EW', pady=15)
+            .grid(row=11, column=0, columnspan=2, sticky='EW', pady=15)
 
     def generate_graph(self):
         try:
@@ -80,12 +89,16 @@ class ExerciseSixTab(BaseTab):
         self.canvas.draw()
         self.print_graph()
 
-    def append_pagerank_info(self, pagerank):
+    def append_pagerank_info(self, pagerank: Dict[int, float]):
         pagerank_string = '\nPageRank:\n'
         for index, pr in sorted(pagerank.items(), key=lambda kv: (kv[1], kv[0]), reverse=True):
             pagerank_string += f'{index}: {pr}\n'
 
         self.append_text(pagerank_string)
+
+    def append_cycle_length_info(self, result_string: str):
+        result_string += str(round(self.graph.calculate_path_length(), 6))
+        self.append_text(result_string)
 
     def random_walk_pagerank(self):
         if self.graph is None:
@@ -109,5 +122,39 @@ class ExerciseSixTab(BaseTab):
         self.print_graph()
         self.append_pagerank_info(pagerank)
 
-    def load_file(self):
-        pass
+    def load_from_file(self):
+        file_path = filedialog.askopenfilename(initialdir='.', title='Wybierz plik', filetypes=[('Pliki danych', '*.dat')])
+        if file_path is None:
+            return
+
+        self.graph = CoordinatedAdjacencyList.from_file(file_path)
+        self.visualization = self.graph.to_networkX()
+        self.axis.clear()
+        pos=nx.get_node_attributes(self.visualization,'pos')
+        nx.draw(self.visualization, pos=pos, ax=self.axis)
+        self.canvas.draw()
+        self.clear_text()
+        self.append_cycle_length_info('Cykl początkowy:\n')
+
+    def simulated_annealing(self):
+        try:
+            max_it = int(self.max_it_entry.get())
+        except ValueError:
+            messagebox.showinfo(title='Wykrzyknik!', message='Wprowadź prawidłowe dane wejściowe!')
+            return
+
+        if max_it < 0:
+            messagebox.showinfo(title='Wykrzyknik!', message='Liczba iteracji nie może być ujemna!')
+            return
+
+        if self.graph is None or not isinstance(self.graph, CoordinatedAdjacencyList):
+            messagebox.showinfo(title='Wykrzyknik!', message='Najpierw musisz wprowadzić poprawny graf!')
+            return
+        
+        self.graph.simulated_annealing(max_it)
+        self.visualization = self.graph.to_networkX()
+        self.axis.clear()
+        pos=nx.get_node_attributes(self.visualization,'pos')
+        nx.draw(self.visualization, pos=pos, ax=self.axis)
+        self.canvas.draw()
+        self.append_cycle_length_info('\nCykl wyjściowy:\n')
---
+++ app/structures/coordinated_adjacency_list.py
@@ -0,0 +1,116 @@
+import structures.adjacency_list as adj_list
+from utils.pythonic import all_equal
+
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import List, Tuple, Dict
+from copy import deepcopy
+import networkx as nx
+import numpy as np
+import random
+import math
+
+@dataclass(eq=True, order=True, frozen=True)
+class Node:
+    index: int
+    x: int
+    y: int
+
+    def __str__(self):
+        return f'({self.index}, x = {self.x}, y = {self.y})' 
+
+class CoordinatedAdjacencyList:
+    def __init__(self, graph):
+        self.graph = graph
+
+    @classmethod
+    def init_empty(cls):
+        return cls(defaultdict(list))
+
+    @classmethod
+    def from_file(cls, file_path: str):
+        graph = defaultdict(list)
+
+        with open(file_path) as file:
+            data_string = file.read()
+
+            index = 0
+            for line in iter(data_string.splitlines()):
+                line = line.split()
+                x = int(line[0])
+                y = int(line[1])
+
+                if index != 0:
+                    previous_node = current_node
+                    current_node = Node(index, x, y)
+                    graph[previous_node].append(current_node)
+                    graph[current_node].append(previous_node)
+                else:
+                    current_node = Node(index, x, y)
+                index += 1
+
+            first_node = list(graph.keys())[0]
+            graph[current_node].append(first_node)
+            graph[first_node].append(current_node)
+
+        return cls(graph)
+
+    def add_edge(self, first_vertex: Node, second_vertex: Node):
+        self.graph[first_vertex].append(second_vertex)
+        self.graph[second_vertex].append(first_vertex)
+
+    def remove_edge(self, first_vertex: Node, second_vertex: Node):
+        self.graph[first_vertex].remove(second_vertex)
+        self.graph[second_vertex].remove(first_vertex)
+
+    def get_vertices(self) -> List[Node]:
+        return self.graph.keys()
+
+    def to_networkX(self) -> nx.Graph:
+        visualization = nx.Graph()
+
+        for vertex in self.get_vertices():
+            visualization.add_node(vertex.index, pos=(vertex.x, vertex.y))
+
+        for start_vertex, end_vertexes in self.graph.items():
+            for end_vertex in end_vertexes:
+                visualization.add_edge(start_vertex.index, end_vertex.index)
+        return visualization
+
+    def calculate_path_length(self) -> float:
+        length = 0
+        previous_vertex = None
+        for start_vertex, vertexes in self.graph.items():
+            end_vertex = vertexes[0] if vertexes[0] != previous_vertex else vertexes[1]
+            previous_vertex = start_vertex
+            length += math.sqrt((start_vertex.x - end_vertex.x)**2 + (start_vertex.y - end_vertex.y)**2)
+
+        return length
+
+    def simulated_annealing(self, max_it: int):
+        for i in range(100, 0, -1):
+            t = 0.001 * i * i
+
+            for _ in range(max_it):
+                new_graph = CoordinatedAdjacencyList(deepcopy(self.graph))
+                while True:
+                    a = random.choice(list(self.get_vertices()))
+                    b = random.choice(self.graph[a])
+                    c = random.choice(list(self.get_vertices()))
+                    d = random.choice(self.graph[c])
+                    if a != c and a != d and b != c and b != d and c not in self.graph[a] and d not in self.graph[b]:
+                        break
+
+                new_graph.remove_edge(a, b)
+                new_graph.add_edge(a, c)
+                new_graph.remove_edge(c, d)
+                new_graph.add_edge(b, d)
+
+                old_graph_length = self.calculate_path_length()
+                new_graph_length = new_graph.calculate_path_length()
+                if new_graph_length < old_graph_length:
+                    self.graph = new_graph.graph
+                else:
+                    r = random.random()
+                    if r < math.exp(-(new_graph_length - old_graph_length) / t):
+                        self.graph = new_graph.graph
